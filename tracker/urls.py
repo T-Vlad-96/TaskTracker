bdd9575648cde6ddf492974b28e026cbd3d1fbbf
user <user@MacBook-Air-user.local>
2026-01-03 19:53:10 +0200

    feat: Add TagUpdateView with url and tempalte. Add links to update view to tags table

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
-from .views import HomeListView, TagListView
+from .views import (
+    HomeListView,
+    TagListView,
+    TagUpdateView,
+)
 
 urlpatterns = [
     path("", HomeListView.as_view(), name="home"),
-    path("tags/", TagListView.as_view(), name="tags")
+    path("tags/", TagListView.as_view(), name="tags"),
+    path(
+        "tags/update/<int:pk>/",
+        TagUpdateView.as_view(),
+        name="tag_update"
+    )
 ]
 
 app_name = "tracker"
--- tracker/views.py
+++ tracker/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, UpdateView
 
 from .models import Task, Tag
-
-
-class HomeListView(ListView):
-    model = Task
-    template_name = "tracker/home.html"
 
 
 class TagListView(ListView):
     model = Tag
     template_name = "tracker/tags.html"
+
+
+class TagUpdateView(UpdateView):
+    model = Tag
+    fields = ("name", )
+    success_url = reverse_lazy("tracker:home")
+
+class HomeListView(ListView):
+    model = Task
+    template_name = "tracker/home.html"
+
